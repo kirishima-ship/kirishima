761d25c5b9ec30865164310e1d5c3fb9189fa11a
KagChi <59391215+KagChi@users.noreply.github.com>
2024-01-16 14:11:38 +0700

    feat: advanced cache driver (#25)
    * feat: surrealdb cache driver
    
    Co-authored-by: Fuyuki Satou <satoufuyuki@proton.me>
    Co-authored-by: Vann <stevanvincent05@gmail.com>
    Co-authored-by: Hazmi35 <contact@hzmi.xyz>
    
    * chore(redis-driver): deps
    
    * feat(redis-driver): add more func
    
    * chore(redis-driver): remove unused spaces
    
    * fix(redis-driver): values not returning value
    
    * feat: using hash for redis
    
    * chore: some optimizsation
    
    * chore: regenerate lockfiles
    
    ---------
    
    Co-authored-by: Fuyuki Satou <satoufuyuki@proton.me>
    Co-authored-by: Vann <stevanvincent05@gmail.com>
    Co-authored-by: Hazmi35 <contact@hzmi.xyz>

---
+++ drivers/redis-driver/src/index.ts
@@ -0,0 +1,72 @@
+import { PlayerCacheDriver, PlayerData } from "@kirishima/core-driver";
+import { Redis, RedisOptions } from "ioredis";
+import { redisScan } from "@nezuchan/utilities";
+
+export class RedisPlayerDriver implements PlayerCacheDriver {
+    public redis: Redis;
+    public constructor(options: RedisOptions) {
+        this.redis = new Redis(options);
+    }
+
+    public async get(clientId: string, guildId: string): Promise<PlayerData | null> {
+        const data = (await this.redis.hgetall(`player:${clientId}:${guildId}`))!;
+
+        const parsedData: any = {};
+        Object.entries(data).forEach(([key, value]) => {
+            try {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                parsedData[key] = JSON.parse(value);
+            } catch {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                parsedData[key] = value;
+            }
+        });
+
+        return parsedData;
+    }
+
+    public set(clientId: string, guildId: string, data: PlayerData): PlayerData {
+        Object.entries(data).forEach(async ([key, value]) => {
+            if (typeof value === "object") {
+                value = JSON.stringify(value);
+            }
+
+            await this.redis.hset(`player:${clientId}:${guildId}`, key, value as string);
+        });
+
+        return data;
+    }
+
+    public async delete(clientId: string, guildId: string): Promise<void> {
+        await this.redis.hdel(`player:${clientId}:${guildId}`);
+    }
+
+    public async values(clientId: string): Promise<PlayerData[]> {
+        const keys = await redisScan(this.redis, clientId, 5000);
+        const values: any[] = [];
+
+        for (const key of keys) {
+            const data = (await this.redis.hgetall(key))!;
+
+            const parsedData: any = {};
+            Object.entries(data).forEach(([keyData, value]) => {
+                try {
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                    parsedData[keyData] = JSON.parse(value);
+                } catch {
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                    parsedData[keyData] = value;
+                }
+            });
+
+            values.push(parsedData);
+        }
+
+        return values;
+    }
+
+    public async size(clientId: string): Promise<number> {
+        const data = await redisScan(this.redis, clientId, 5000);
+        return data.length;
+    }
+}
